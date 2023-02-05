5f2fb645b6f58c2f835f9980c2b1da4903f27108
seifeslimene <s.slimene19@gmail.com>
2023-02-05 14:12:46 +0100

    initial commit

---
+++ client/src/components/ExpenseItem.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { ListGroupItem, Badge, Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+
+const ExpenseItem = ({ item, onDelete }) => {
+  return (
+    <ListGroupItem
+      className="list-group-item"
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+    >
+      <div className="float-left">
+        <span style={{ marginRight: 5 }}>
+          {item.description ? item.description : 'Expense Item'}
+        </span>
+        <Badge color="dark">${item.amount}</Badge>
+        <div className="text-muted">{moment(item.created).format('LL')}</div>
+      </div>
+      <div className="float-right">
+        <Link
+          to={{
+            pathname: '/edit',
+            state: { item },
+          }}
+          className="btn btn-secondary btn-sm"
+        >
+          Edit
+        </Link>
+        &nbsp;
+        <Button color="danger" size="sm" onClick={onDelete} data-id={item._id}>
+          Delete
+        </Button>
+      </div>
+    </ListGroupItem>
+  );
+};
+
+export { ExpenseItem };
---
+++ server/index.js
@@ -0,0 +1,9 @@
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const app = require('./src/app');
+
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => {
+  console.log(`Server is ready for connections on port ${PORT}`);
+});
